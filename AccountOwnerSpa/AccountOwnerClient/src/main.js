1c6b7b02af89990f4f811ce3099a4c7d9d050046
Kevin <fungai.nyatanga16@gamil.com>
2019-04-17 13:22:47 -0500

    vue routing and navigation

--- AccountOwnerSpa/AccountOwnerClient/src/main.js
+++ AccountOwnerSpa/AccountOwnerClient/src/main.js
@@ -3,10 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
+// Vue.use() is the function which is used to install vue plugins. It will trigger the install function inside a plugin
+// In this case, the Vue.use() function will trigger the install function inside the BootstrapVue plugin and that function
+// will register all the Bootstrap components for us.
+
+Vue.use(BootstrapVue)
+
 new Vue({
   el: '#app',
   router,
